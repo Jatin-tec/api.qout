8023befdcecb696e68c2e0cbfc9bcd66263815ba
jatin.kshatriya <jatin.kshatriya@tcs.com>
2025-04-15 05:37:44 +0000

    setting updated

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', False)
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 HOST = os.environ.get("HOST", default="http://localhost:8000")
